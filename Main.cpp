14aeb684c64b3b37969e5ab8c618eae206db74c8
Sejusa <110556472+Sejusa@users.noreply.github.com>
2025-09-22 22:47:34 +0200

    Hello_World V. 1.0.0

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-char letra;
+#include <iostream> //El #include es para añadir librerías.
 
-void main() 
-{
-	letra = 'S';
-	std::cout << letra;
+void main() {
+
+	std::cout << "Mi nombre es: Sergi Funes Ortiz" << std::endl; //<< para sacar por pantalla.
+	system("pause"); //Para esperar a pulsar una tecla y no se cierre de golpe.
+
 }
