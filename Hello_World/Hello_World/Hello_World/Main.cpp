c83d78c2aac72fce48454efa4130f01b7970a6ee
Sejusa <110556472+Sejusa@users.noreply.github.com>
2025-09-22 22:38:57 +0200

    Delete Hello_World/Hello_World directory

--- Hello_World/Hello_World/Hello_World/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> //El #include es para añadir librerías.
-
-void main() {
-
-	std::cout << "Mi nombre es: Sergi Funes Ortiz" << std::endl; //<< para sacar por pantalla.
-	system("pause"); //Para esperar a pulsar una tecla y no se cierre de golpe.
-
-}
