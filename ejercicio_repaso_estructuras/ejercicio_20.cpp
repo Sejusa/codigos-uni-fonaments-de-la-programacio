6b3283a308f09ab6682adc8211010f207b207d9d
Sejusa <110556472+Sejusa@users.noreply.github.com>
2025-10-05 10:05:27 +0200

    V. 1.0.0

--- ejercicio_repaso_estructuras/ejercicio_20.cpp
+++ ejercicio_repaso_estructuras/ejercicio_20.cpp
@@ -9,13 +9,17 @@ cliente.
 
 void main() {
 	bool quieresIngrediente;
-	std::string ingredientes[6] = {"queso", "bacon", "lechuga", "tomate", "atun", "aceite"};
+	std::string ingredientes[6] = { "queso", "bacon", "lechuga", "tomate", "atun", "aceite" };
+	std::string bocadillo; //Aquí almazenaremos todos los ingredientes que el ususario quiere.
 
 	for (short i = 0; i < 6; i++) {
-		switch (i) {
-			case 0:
-				std::cout << "Quieres " << ingredientes[i] << " ? 0 = No   1 = Si" << std::endl;
-				std::cin >> quieresIngrediente;
+		std::cout << "Quieres " << ingredientes[i] << " ? 0 = No   1 = Si" << std::endl; //Usando la variable i accedemos a cada valor de la array.
+		std::cin >> quieresIngrediente;
+
+		if (quieresIngrediente == true) {
+			bocadillo += " " + ingredientes[i]; //Añadimos el ingrediente de la array actual según i a la lista.
 		}
 	}
+
+	std::cout << "Tu bocadillo tiene: " << bocadillo;
 }
